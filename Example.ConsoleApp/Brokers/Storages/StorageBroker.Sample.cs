b86dd9c8fbfd75feb185bacd3e5c52959e4a4d32
evangelinedrink <Evangelinedrink@gmail.com>
2022-04-04 23:24:27 -0700

    BROKER: Insert Sample

---
+++ Example.ConsoleApp/Brokers/Storages/StorageBroker.Sample.cs
@@ -0,0 +1,19 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Example.ConsoleApp.Models.Samples;
+
+namespace Example.ConsoleApp.Brokers.Storages
+{
+    internal partial class StorageBroker : IStorageBroker
+    {
+        List<Sample> Samples = new List<Sample>();
+        public Sample InsertSample(Sample sample)
+        {
+            Samples.Add(sample);
+
+            return sample;
+        }
+    }
+}
---
+++ Example.ConsoleApp/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,10 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+namespace Example.ConsoleApp.Brokers.Storages
+{
+    internal partial class StorageBroker : IStorageBroker
+    {
+    }
+}
