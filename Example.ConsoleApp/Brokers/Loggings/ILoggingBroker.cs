9bc524374e8ef34b64bc663cc51a31bc34b9f971
evangelinedrink <Evangelinedrink@gmail.com>
2022-04-18 23:57:19 -0700

    BROKER: Setting Up Logging Broker

---
+++ Example.ConsoleApp/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.ConsoleApp.Brokers.Loggings
+{
+    internal interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+    }
+}
---
+++ Example.ConsoleApp/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Example.ConsoleApp.Brokers.Loggings
+{
+    internal class LoggingBroker : ILoggingBroker
+    {
+        public ILogger logger { get; set; }
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+        
+        public void LogError(Exception exception) =>
+             throw new NotImplementedException();
+        
+    }
+}
